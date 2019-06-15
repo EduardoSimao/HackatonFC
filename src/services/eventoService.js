28650c779fc305f06f45d1f3ffc6b0d711ec2c73
Carolina Carvalho <carool.carvalhoo05@gmail.com>
2019-06-15 15:56:36 -0300

    Insercao de data final e data inicial do evento

--- src/services/eventoService.js
+++ src/services/eventoService.js
@@ -4,6 +4,8 @@ const eventoSchema = new mongoose.Schema({
     tipo: { type: String, required: true},  
     author:{ type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true },
     desc: { type: String, required: true},
+    DataInicial: { type: Date, required: true},
+    DataFinal: { type: Date, required: false},
     createAt: {type: Date, default: Date.now},
     situacao: {type: Boolean, default: true}
 });
